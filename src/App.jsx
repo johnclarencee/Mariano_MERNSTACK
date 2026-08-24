1818999b26f3b17a71ca7691dcd4e311e76a1a57
johnclarencee <akohappy48@gmail.com>
2026-08-24 08:57:29 +0800

    subjectDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Student from './components/student.jsx'
+import Subject from './components/subject.jsx'
 
 function App() {
   return (
+    <>
     <div>
       <Student name="Clarence Mariano" age="20" section="BSIT 3-1" student_number="202400197" course="Information Technology" />
       </div>
+
+<br></br>
+<hr></hr>
+<br></br>
+
+     <div>
+       <Subject name="Application Development and Emerging Technologies" code="20260534" units="3" />
+      </div>
+      </>
   )
 }
+
 
 export default App
